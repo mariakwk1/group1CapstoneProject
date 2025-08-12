94b12dea7549c36504a600a2daf37dc43b76e645
Eleanor Abitbol <eleanor.abitbol@gmail.com>
2025-08-12 09:55:51 -0400

    added stuff

---
+++ group1CapstoneProject/Menu View.swift
@@ -0,0 +1,21 @@
+//
+//  Menu View.swift
+//  group1CapstoneProject
+//
+//  Created by Scholar on 8/12/25.
+//
+
+import SwiftUI
+
+struct Menu_View: View {
+    @Binding var menu: menu
+    var body: some View {
+        VStack{
+            TextField("menu", text:)
+        }//end of Vstack
+    }//end of var
+}//end of struct
+
+#Preview {
+    Menu_View()
+}
