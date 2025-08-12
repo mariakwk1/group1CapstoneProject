51b18bbe963a5df393802e72f0e7ad23d569e29d
maria <mariae8819550@gmail.com>
2025-08-12 09:34:20 -0400

    Initial Commit

---
+++ group1CapstoneProject/group1CapstoneProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  group1CapstoneProjectApp.swift
+//  group1CapstoneProject
+//
+//  Created by Scholar on 8/12/25.
+//
+
+import SwiftUI
+
+@main
+struct group1CapstoneProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
