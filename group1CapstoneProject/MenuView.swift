24ac59922da41f2f225ce5f47ac000037309e9e3
Eleanor Abitbol <eleanor.abitbol@gmail.com>
2025-08-12 11:38:41 -0400

    .

--- group1CapstoneProject/MenuView.swift
+++ group1CapstoneProject/MenuView.swift
@@ -10,7 +10,11 @@ import SwiftUI
 struct Menu_View: View {
     var body: some View {
         VStack{
-    
+            TextField("Menu", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
+                .background(Color(.systemGroupedBackground))
+                .cornerRadius(10)
+                .padding()
+                Spacer()
         }//end of Vstack
     }//end of var
 }//end of struct
