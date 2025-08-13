2f41e65234dfe2d0cae920f440d77bff4fffbbd5
Valerie <valeriealexarivera@gmail.com>
2025-08-13 14:47:03 -0400

    added background to quote of the day

--- group1CapstoneProject/QuoteView.swift
+++ group1CapstoneProject/QuoteView.swift
@@ -10,18 +10,29 @@ import SwiftUI
 struct QuoteView: View {
     var body: some View {
         ZStack{
-            Color(hue: 0.10, saturation: 0.234, brightness: 0.871, opacity: 0.475)
-                .ignoresSafeArea()
+                Color(hue: 0.10, saturation: 0.234, brightness: 0.871, opacity: 0.475)
+                    .ignoresSafeArea()
             VStack{
-                Text("Quote Of The Day ")
+                Text("Quote Of The Day")
+                    .padding()
                     .font(.largeTitle)
-                    .fontWeight(.medium)
-                    .background(Rectangle().foregroundColor(.white))
-    
-        }//end VStack
-            
-        }// end ZStack
-        
+                    .fontWeight(.bold)
+                    .padding(.horizontal, 30)
+                    .padding(.vertical, 15)
+                    .background(
+                    Capsule()
+                  .fill(Color.white.opacity(0.7))
+                                        )
+                  .foregroundColor(.black)
+                .padding(.top, 40)
+                Spacer()
+               
+                Text("If you dont risk anything, you risk even more. - Erica Jong")
+                    .font(.title)
+                
+                   
+            }
+        }
     }
 }
 #Preview {
