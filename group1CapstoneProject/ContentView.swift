a314c21f91154f0f5eb4edfceca4c521a3409990
maria <mariae8819550@gmail.com>
2025-08-14 15:01:40 -0400

    debugged content view

--- group1CapstoneProject/ContentView.swift
+++ group1CapstoneProject/ContentView.swift
@@ -6,93 +6,66 @@
 //
 
 import SwiftUI
-
 struct ContentView: View {
-    @State private var viewMenu="Menu"
-    
-    var body: some View {
+  @State private var viewMenu="Menu"
+  var body: some View {
     NavigationStack {
-    ZStack {
-     // Full screen background
+      ZStack {
+        // Full screen background
         Color(hue: 0.10, saturation: 0.234, brightness: 0.871, opacity: 0.475)
-            .ignoresSafeArea()
-        Spacer()
+          .ignoresSafeArea()
         NavigationLink(destination: MenuView(menu: $viewMenu)) {
-                    Image("human_brain-removebg-preview")
-                    }
-                }//end of zstack
-            }//end vstack
-            Text("MindMosaic")
-        
-       // NavigationLink(destination: secondView()) {
-            Text("Click Icon for menu")
-        }//end navstacl
-           
-        }//end navlink
-/*
-          NavigationStack {
-              ZStack {
-                  // Full screen background
-                  Color(hue: 0.10, saturation: 0.234, brightness: 0.871, opacity: 0.475)
-                      .ignoresSafeArea()
-                  NavigationLink(destination: MenuView(menu: $viewMenu))
-                  {
-                      Image("human_brain-removebg-preview")
-                  }
-                  Text("MindMosaic")
-                      .font(.largeTitle)
-                      .fontWeight(.semibold)
-                      .padding(.bottom, 600)
-                  
-                  
-                  
-                  // NavigationLink(destination: secondView()) {
-                  Text("Click Icon for menu")
-                      .font(.headline)
-                      .fontWeight(.semibold)
-                      .padding(.top, 700)
-              }
-            }//end navstac
-            */
-       
-
-
-
-
-      
-#Preview {
-           ContentView()
+          Image("human_brain-removebg-preview")
         }
+        Text("MindMosaic")
+          .font(.largeTitle)
+          .fontWeight(.semibold)
+          .padding(.bottom, 600)
+        // NavigationLink(destination: secondView()) {
+        Text("Click :brain: for menu")
+          .font(.headline)
+          .fontWeight(.semibold)
+          .padding(.top, 700)
+      }//end navstac
+    }//end of zstack
+  }//end navlink]
+}
+#Preview {
+      ContentView()
+    }
 ////end content
 //=======
-//    Text("MindMosaic")
-//    .font(.largeTitle)
-//    .fontWeight(.bold)
-//
-//    ZStack {
-//   Image("human_brain-removebg-preview")
-//   .resizable()
-//   .aspectRatio(contentMode: .fit)
-//   .frame(maxWidth: 300) // Adjust as needed
-//        }// end ZStack
-//
-//  Text("Click 🧠 for menu")
-//  .font(.headline)
+//  Text("MindMosaic")
+//  .font(.largeTitle)
 //  .fontWeight(.bold)
-//  Spacer()
+//
+//  ZStack {
+//  Image("human_brain-removebg-preview")
+//  .resizable()
+//  .aspectRatio(contentMode: .fit)
+//  .frame(maxWidth: 300) // Adjust as needed
+//    }// end ZStack
+//
+// Text("Click :brain: for menu")
+// .font(.headline)
+// .fontWeight(.bold)
+// Spacer()
 // }// end VStack
 //}// end ZStack
 //}// end navigationStack
 //}// end body
 //}// end contentView
 //
-//        #Preview {
-//            ContentView()
-//        }
+//    #Preview {
+//      ContentView()
+//    }
 //>>>>>>> main
 //
 //}
 //}
 //}
 //}
-          //}*/
+//}
+
+
+
